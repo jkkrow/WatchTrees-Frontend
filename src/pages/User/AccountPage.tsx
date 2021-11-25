fa86e7216065dc811ce3a1d964a16777e27cacac
unknown <dlord4242@gmail.com>
2021-11-25 17:23:21 +0900

    Fixed AccountPage

--- src/pages/User/AccountPage.tsx
+++ src/pages/User/AccountPage.tsx
@@ -1,10 +1,45 @@
+import Button from 'components/Common/Element/Button/Button';
+import Modal from 'components/Common/UI/Modal/Modal';
 import UserLayout from 'components/User/Layout/UserLayout';
-import AccountProfile from 'components/User/Account/Profile/AccountProfile';
+import AccountDashboard from 'components/User/Account/Dashboard/AccountDashboard';
+import { useAppDispatch, useAppSelector } from 'hooks/store-hook';
+import { authActions } from 'store/slices/auth-slice';
+import { sendVerification } from 'store/thunks/auth-thunk';
 
 const AccountPage: React.FC = () => {
+  const { userData, loading, error, message } = useAppSelector(
+    (state) => state.auth
+  );
+
+  const dispatch = useAppDispatch();
+
+  const verifyEmailHandler = () => {
+    dispatch(sendVerification(userData!.email));
+  };
+
+  const closeModalHandler = () => {
+    dispatch(authActions.clearResponse());
+  };
+
   return (
     <UserLayout>
-      <AccountProfile />
+      <Modal
+        on={!!error || !!message}
+        type="message"
+        header={error ? 'Error' : 'Email has sent'}
+        onClose={closeModalHandler}
+      >
+        <div>{error || message}</div>
+      </Modal>
+      <AccountDashboard />
+      {!userData!.isVerified && (
+        <Button loading={loading} onClick={verifyEmailHandler}>
+          Verify Email
+        </Button>
+      )}
+      {userData!.isVerified && !userData!.isPremium && (
+        <Button>Upgrade to Premium</Button>
+      )}
     </UserLayout>
   );
 };
