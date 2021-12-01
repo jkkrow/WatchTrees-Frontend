b5512f8734ee914a566a146cf40b10a243fd2274
unknown <dlord4242@gmail.com>
2021-12-01 17:43:39 +0900

    Implemented VideoList

--- src/components/Video/List/VideoList.tsx
+++ src/components/Video/List/VideoList.tsx
@@ -1,39 +1,73 @@
 import { useEffect, useRef, useState } from 'react';
+import { useHistory } from 'react-router';
 
+import VideoItem from '../Item/VideoItem';
 import LoadingCard from 'components/Common/UI/Loader/Card/LoadingCard';
+import Pagination from 'components/Common/UI/Pagination/Pagination';
+import { usePaginate } from 'hooks/page-hook';
+import { useAppDispatch } from 'hooks/store-hook';
+import { VideoTreeWithCreatorInfo } from 'store/slices/video-slice';
+import { fetchVideos } from 'store/thunks/video-thunk';
 import './VideoList.scss';
 
 const VideoList: React.FC = () => {
-  const [loadingStatus, setLoadingStatus] = useState(true);
-  const [loaderNumber, setLoaderNumber] = useState([1]);
+  const dispatch = useAppDispatch();
+
+  const [loading, setLoading] = useState(false);
+  const [loaders, setLoaders] = useState([undefined]);
+  const [videos, setVideos] = useState<VideoTreeWithCreatorInfo[]>([]);
+  const [count, setCount] = useState(0);
 
   const listRef = useRef<HTMLDivElement>(null);
   const itemRef = useRef<HTMLDivElement>(null);
 
+  const { currentPage, itemsPerPage } = usePaginate(20);
+
+  const history = useHistory();
+
   useEffect(() => {
-    const listWidth = listRef.current!.offsetWidth;
-    const itemWidth = itemRef.current!.offsetWidth;
+    (async () => {
+      setLoading(true);
 
-    let rows = Math.floor(listWidth / itemWidth);
+      const listWidth = listRef.current!.offsetWidth;
+      const itemWidth = itemRef.current!.offsetWidth;
 
-    if (rows === 1) rows = 3;
+      let rows = Math.floor(listWidth / itemWidth);
 
-    setLoaderNumber(Array.from(Array(rows * 3)));
-  }, []);
+      setLoaders(Array.from(Array(rows * 3)));
+
+      const data = await dispatch(
+        fetchVideos(
+          { page: currentPage, max: itemsPerPage },
+          history.action !== 'POP'
+        )
+      );
+
+      if (data) {
+        setVideos(data.videos);
+        setCount(data.count);
+        setLoading(false);
+      }
+    })();
+  }, [dispatch, history, currentPage, itemsPerPage]);
 
   return (
-    <div className="video-list" ref={listRef}>
-      {loaderNumber.map((item, index) => (
-        <div
-          key={index}
-          className="video-item"
-          ref={itemRef}
-          onClick={() => setLoadingStatus(false)}
-        >
-          <LoadingCard on={loadingStatus} detail />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="video-list" ref={listRef}>
+        {loaders.map((item, index) => (
+          <div key={index} className="video-item" ref={itemRef}>
+            <LoadingCard on={loading} detail />
+          </div>
+        ))}
+        {!loading &&
+          videos.map((item) => <VideoItem key={item._id} video={item} />)}
+      </div>
+      <Pagination
+        count={count}
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+      />
+    </>
   );
 };
 
