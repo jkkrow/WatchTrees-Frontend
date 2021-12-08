ab1cda36760818c789003dd7617d43014af27ebb
unknown <dlord4242@gmail.com>
2021-12-08 17:07:04 +0900

    Moved to UI sub folder

--- src/components/Video/UI/Thumbnail/VideoThumbnail.tsx
+++ src/components/Video/UI/Thumbnail/VideoThumbnail.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router';
+
 import { ReactComponent as PreviewIcon } from 'assets/icons/preview.svg';
 import { VideoTree } from 'store/slices/video-slice';
 import { thumbanilUrl } from 'util/video';
@@ -8,8 +10,14 @@ interface VideoThumbnailProps {
 }
 
 const VideoThumbnail: React.FC<VideoThumbnailProps> = ({ video }) => {
+  const history = useHistory();
+
+  const watchVideoHandler = () => {
+    history.push(`/video/${video._id}`);
+  };
+
   return (
-    <div className="video-thumbnail">
+    <div className="video-thumbnail" onClick={watchVideoHandler}>
       {thumbanilUrl(video) ? (
         <img src={thumbanilUrl(video)} alt={video.info.title} />
       ) : (
