d206250e007d470f1a0cbd904fb32cf6bc159583
unknown <dlord4242@gmail.com>
2021-12-09 16:52:06 +0900

    Added link function to tags

--- src/components/Video/UI/Tags/VideoTags.tsx
+++ src/components/Video/UI/Tags/VideoTags.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router';
+
 import './VideoTags.scss';
 
 interface TagsProps {
@@ -5,10 +7,20 @@ interface TagsProps {
 }
 
 const VideoTags: React.FC<TagsProps> = ({ tags }) => {
+  const history = useHistory();
+
+  const searchHandler = (tag: string) => {
+    history.push(`/?search=${tag}`);
+  };
+
   return tags.length > 0 ? (
     <div className="video-tags">
       {tags.map((tag) => (
-        <div key={tag} className="video-tag">
+        <div
+          key={tag}
+          className="video-tag link"
+          onClick={() => searchHandler(tag)}
+        >
           #{tag}
         </div>
       ))}
