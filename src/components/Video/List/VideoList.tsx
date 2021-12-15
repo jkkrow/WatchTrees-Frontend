12637f8d4ad0e609b7f27d4569c4774ab497f8c6
unknown <dlord4242@gmail.com>
2021-12-15 17:35:50 +0900

    Removed params props

--- src/components/Video/List/VideoList.tsx
+++ src/components/Video/List/VideoList.tsx
@@ -6,31 +6,25 @@ import VideoLoader from 'components/Common/UI/Loader/Video/VIdeoLoader';
 import Pagination from 'components/Common/UI/Pagination/Pagination';
 import { usePaginate } from 'hooks/page-hook';
 import { useSearch } from 'hooks/search-hook';
-import { useAppDispatch } from 'hooks/store-hook';
 import { VideoListDetail } from 'store/slices/video-slice';
 import './VideoList.scss';
 
 interface VideoListProps {
-  params?: {
-    max?: number;
-    userId?: string;
-  };
   onFetch: (
     params: any,
     forceUpdate: boolean
   ) => Promise<{ videos: VideoListDetail[]; count: number }>;
 }
 
-const VideoList: React.FC<VideoListProps> = ({ params, onFetch }) => {
+const VideoList: React.FC<VideoListProps> = ({ onFetch }) => {
   const [videos, setVideos] = useState<VideoListDetail[]>([]);
   const [count, setCount] = useState(0);
   const [loaders, setLoaders] = useState<undefined[]>([]);
   const [loaded, setLoaded] = useState(false);
 
-  const { currentPage, itemsPerPage } = usePaginate(params?.max || 20);
+  const { currentPage, itemsPerPage } = usePaginate(20);
   const { keyword } = useSearch();
 
-  const dispatch = useAppDispatch();
   const listRef = useRef<HTMLDivElement>(null);
   const history = useHistory();
 
@@ -41,25 +35,24 @@ const VideoList: React.FC<VideoListProps> = ({ params, onFetch }) => {
 
       setLoaders(Array.from(Array(rows * 3)));
 
-      const { videos, count } = await onFetch(
+      const data = await onFetch(
         {
           page: currentPage,
           max: itemsPerPage,
           search: keyword,
-          ...params,
         },
         history.action !== 'POP'
       );
 
-      if (videos && count) {
-        setVideos(videos);
-        setCount(count);
+      if (data) {
+        setVideos(data.videos);
+        setCount(data.count);
       }
 
       setLoaders([]);
       setLoaded(true);
     })();
-  }, [dispatch, history, currentPage, itemsPerPage, keyword, params, onFetch]);
+  }, [history, currentPage, itemsPerPage, keyword, onFetch]);
 
   return (
     <>
