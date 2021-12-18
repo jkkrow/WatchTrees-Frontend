0596b066e8273082ef338575431590346092a22e
unknown <dlord4242@gmail.com>
2021-12-18 16:49:29 +0900

    Added video preview to VideoThumbnail

--- src/components/Video/UI/Thumbnail/VideoThumbnail.tsx
+++ src/components/Video/UI/Thumbnail/VideoThumbnail.tsx
@@ -1,8 +1,11 @@
+import { useRef, useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
+import shaka from 'shaka-player';
 
 import { ReactComponent as PreviewIcon } from 'assets/icons/preview.svg';
+import { useTimeout } from 'hooks/timer-hook';
 import { VideoTree } from 'store/slices/video-slice';
-import { thumbanilUrl } from 'util/video';
+import { videoUrl, thumbanilUrl } from 'util/video';
 import './VideoThumbnail.scss';
 
 interface VideoThumbnailProps {
@@ -10,16 +13,77 @@ interface VideoThumbnailProps {
 }
 
 const VideoThumbnail: React.FC<VideoThumbnailProps> = ({ video }) => {
+  const [isVideoPlay, setIsVideoPlay] = useState(false);
+
+  const [thumbnailTimeout, clearThumbnailTimeout] = useTimeout();
+
+  const videoRef = useRef<HTMLVideoElement | null>(null);
   const history = useHistory();
+
+  useEffect(() => {
+    (async () => {
+      const videoElement = videoRef.current;
+
+      if (!videoElement || !video.root.info) return;
+
+      const src = videoUrl(video.root.info.url, video.root.info.isConverted);
+      const player = new shaka.Player(videoElement);
+
+      await player.load(src);
+    })();
+  }, [video]);
 
   const watchVideoHandler = () => {
     history.push(`/video/${video._id}`);
   };
 
+  const thumbnailHoverHandler = () => {
+    thumbnailTimeout(() => {
+      const videoElement = videoRef.current;
+
+      setIsVideoPlay(true);
+      videoElement && videoElement.play();
+    }, 800);
+  };
+
+  const thumbnailHoverOutHandler = () => {
+    const videoElement = videoRef.current;
+
+    setIsVideoPlay(false);
+    clearThumbnailTimeout();
+
+    if (videoElement) {
+      videoElement.pause();
+      videoElement.currentTime = 0;
+    }
+  };
+
+  const timeChangeHandler = () => {
+    const videoElement = videoRef.current!;
+
+    if (videoElement.currentTime > 10) {
+      videoElement.currentTime = 0;
+    }
+  };
+
   return (
-    <div className="video-thumbnail" onClick={watchVideoHandler}>
+    <div
+      className="video-thumbnail"
+      onClick={watchVideoHandler}
+      onMouseEnter={thumbnailHoverHandler}
+      onMouseLeave={thumbnailHoverOutHandler}
+    >
       {thumbanilUrl(video) ? (
-        <img src={thumbanilUrl(video)} alt={video.info.title} />
+        <>
+          {video.root.info && (
+            <video muted loop ref={videoRef} onTimeUpdate={timeChangeHandler} />
+          )}
+          <img
+            style={{ opacity: isVideoPlay ? 0 : 1 }}
+            src={thumbanilUrl(video)}
+            alt={video.info.title}
+          />
+        </>
       ) : (
         <PreviewIcon />
       )}
