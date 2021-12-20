2ae42b74751c5be49cf21e90b27909a3632121d1
unknown <dlord4242@gmail.com>
2021-12-20 17:48:16 +0900

    Added week to formatDate

--- src/util/format.ts
+++ src/util/format.ts
@@ -55,6 +55,7 @@ export const formatDate = (date: string | Date) => {
   const intervals = [
     { label: 'year', seconds: 31536000 },
     { label: 'month', seconds: 2592000 },
+    { label: 'week', seconds: 604800 },
     { label: 'day', seconds: 86400 },
     { label: 'hour', seconds: 3600 },
     { label: 'minute', seconds: 60 },
