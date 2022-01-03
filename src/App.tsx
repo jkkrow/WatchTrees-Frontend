ab2a40b80a3d453afb4dd429c86e408291abc948
unknown <dlord4242@gmail.com>
2022-01-03 09:48:28 +0900

    Added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Header from 'components/Layout/Header/Header';
 import Footer from 'components/Layout/Footer/Footer';
 import GlobalMessageList from 'components/Common/UI/GlobalMessage/List/GlobalMessageList';
-import VideoPage from 'pages/Video/VideoPage';
-import VideoListPage from 'pages/Video/VideoListPage';
-import MyVideoListPage from 'pages/Video/MyVideoListPage';
-import HistoryPage from 'pages/Video/HistoryPage';
-import ChannelPage from 'pages/Video/ChannelPage';
-import UploadPage from 'pages/Upload/UploadPage';
-import LoginPage from 'pages/User/LoginPage';
-import AccountPage from 'pages/User/AccountPage';
-import VerificationPage from 'pages/User/VerificationPage';
-import SendRecoveryPage from 'pages/User/SendRecoveryPage';
-import ResetPasswordPage from 'pages/User/ResetPasswordPage';
-import NotFoundPage from 'pages/Error/NotFoundPage';
 import ProtectedRoute from 'service/ProtectedRoute';
 import { useAppDispatch, useAppSelector } from 'hooks/store-hook';
 import { fetchTokenOnload } from 'store/thunks/user-thunk';
 import './App.scss';
-import FavoritesPage from 'pages/Video/FavoritesPage';
+
+const VideoPage = lazy(() => import('pages/Video/VideoPage'));
+const VideoListPage = lazy(() => import('pages/Video/VideoListPage'));
+const MyVideoListPage = lazy(() => import('pages/Video/MyVideoListPage'));
+const HistoryPage = lazy(() => import('pages/Video/HistoryPage'));
+const ChannelPage = lazy(() => import('pages/Video/ChannelPage'));
+const UploadPage = lazy(() => import('pages/Upload/UploadPage'));
+const LoginPage = lazy(() => import('pages/User/LoginPage'));
+const AccountPage = lazy(() => import('pages/User/AccountPage'));
+const VerificationPage = lazy(() => import('pages/User/VerificationPage'));
+const SendRecoveryPage = lazy(() => import('pages/User/SendRecoveryPage'));
+const ResetPasswordPage = lazy(() => import('pages/User/ResetPasswordPage'));
+const NotFoundPage = lazy(() => import('pages/Error/NotFoundPage'));
+const FavoritesPage = lazy(() => import('pages/Video/FavoritesPage'));
 
 const App: React.FC = () => {
   const { refreshToken } = useAppSelector((state) => state.user);
-
   const { dispatch } = useAppDispatch();
 
   useEffect(() => {
@@ -33,76 +33,78 @@ const App: React.FC = () => {
 
   return (
     <BrowserRouter>
-      <GlobalMessageList />
-      <Header />
-      <main>
-        <Switch>
-          <Route component={VideoListPage} exact path="/" />
-          <Route component={VideoPage} exact path="/video/:id" />
-          <Route component={ChannelPage} exact path="/channel/:id" />
-          <Route component={HistoryPage} exact path="/history" />
-          <Route
-            component={VerificationPage}
-            exact
-            path="/auth/verification/:token"
-          />
-          <ProtectedRoute
-            require={!refreshToken}
-            component={LoginPage}
-            exact
-            path="/auth"
-          />
-          <ProtectedRoute
-            require={!refreshToken}
-            component={SendRecoveryPage}
-            exact
-            path="/auth/recovery"
-          />
-          <ProtectedRoute
-            require={!refreshToken}
-            component={ResetPasswordPage}
-            exact
-            path="/auth/reset-password/:token"
-          />
-          <ProtectedRoute
-            require={refreshToken}
-            component={AccountPage}
-            exact
-            path="/user/account"
-            redirect="/auth"
-          />
-          <ProtectedRoute
-            require={refreshToken}
-            component={MyVideoListPage}
-            exact
-            path="/user/videos"
-            redirect="/auth"
-          />
-          <ProtectedRoute
-            require={refreshToken}
-            component={FavoritesPage}
-            exact
-            path="/user/favorites"
-            redirect="/auth"
-          />
-          <ProtectedRoute
-            require={refreshToken}
-            component={UploadPage}
-            exact
-            path="/upload"
-            redirect="/auth"
-          />
-          <ProtectedRoute
-            require={refreshToken}
-            component={UploadPage}
-            exact
-            path="/upload/:id"
-            redirect="/auth"
-          />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </main>
-      <Footer />
+      <Suspense fallback={<div />}>
+        <GlobalMessageList />
+        <Header />
+        <main>
+          <Switch>
+            <Route component={VideoListPage} exact path="/" />
+            <Route component={VideoPage} exact path="/video/:id" />
+            <Route component={ChannelPage} exact path="/channel/:id" />
+            <Route component={HistoryPage} exact path="/history" />
+            <Route
+              component={VerificationPage}
+              exact
+              path="/auth/verification/:token"
+            />
+            <ProtectedRoute
+              require={!refreshToken}
+              component={LoginPage}
+              exact
+              path="/auth"
+            />
+            <ProtectedRoute
+              require={!refreshToken}
+              component={SendRecoveryPage}
+              exact
+              path="/auth/recovery"
+            />
+            <ProtectedRoute
+              require={!refreshToken}
+              component={ResetPasswordPage}
+              exact
+              path="/auth/reset-password/:token"
+            />
+            <ProtectedRoute
+              require={refreshToken}
+              component={AccountPage}
+              exact
+              path="/user/account"
+              redirect="/auth"
+            />
+            <ProtectedRoute
+              require={refreshToken}
+              component={MyVideoListPage}
+              exact
+              path="/user/videos"
+              redirect="/auth"
+            />
+            <ProtectedRoute
+              require={refreshToken}
+              component={FavoritesPage}
+              exact
+              path="/user/favorites"
+              redirect="/auth"
+            />
+            <ProtectedRoute
+              require={refreshToken}
+              component={UploadPage}
+              exact
+              path="/upload"
+              redirect="/auth"
+            />
+            <ProtectedRoute
+              require={refreshToken}
+              component={UploadPage}
+              exact
+              path="/upload/:id"
+              redirect="/auth"
+            />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </main>
+        <Footer />
+      </Suspense>
     </BrowserRouter>
   );
 };
--- src/components/Common/UI/Loader/LoadingSpinner.tsx
+++ src/components/Common/UI/Loader/LoadingSpinner.tsx
@@ -2,11 +2,14 @@ import { ReactComponent as RectIcon } from 'assets/icons/rect.svg';
 import './LoadingSpinner.scss';
 
 interface LoadingSpinnerProps {
-  on: boolean;
+  on?: boolean;
   overlay?: boolean;
 }
 
-const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ on, overlay }) =>
+const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
+  on = true,
+  overlay,
+}) =>
   on ? (
     <div className={`loading-spinner${overlay ? ' overlay' : ''}`}>
       <RectIcon className="loading-spinner__1" />
