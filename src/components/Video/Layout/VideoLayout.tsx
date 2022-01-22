fdf1be6c8a6925e0bbbe8a9ae6d97ae7e85cd5a1
unknown <dlord4242@gmail.com>
2022-01-22 19:51:35 +0900

    Moved fetching data function to VideoLayout from VideoPage

--- src/components/Video/Layout/VideoLayout.tsx
+++ src/components/Video/Layout/VideoLayout.tsx
@@ -1,98 +1,108 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router';
+import { useEffect } from 'react';
+import { useHistory, useParams } from 'react-router';
 
 import VideoTree from 'components/Video/TreeView/Tree/VideoTree';
-import VideoFavorites from '../UI/Favorites/VideoFavorites';
+import VideoFavorites from 'components/Video/UI/Favorites/VideoFavorites';
 import VideoTags from 'components/Video/UI/Tags/VideoTags';
-import VideoViews from '../UI/Views/VideoViews';
-import VideoDuration from '../UI/Duration/VideoDuration';
-import VideoTimestamp from '../UI/Timestamp/VideoTimestamp';
+import VideoViews from 'components/Video/UI/Views/VideoViews';
+import VideoDuration from 'components/Video/UI/Duration/VideoDuration';
+import VideoTimestamp from 'components/Video/UI/Timestamp/VideoTimestamp';
 import Avatar from 'components/Common/UI/Avatar/Avatar';
+import Loader from '../Player/UI/Loader/Loader';
 import { useAppDispatch, useAppSelector } from 'hooks/store-hook';
 import { VideoItemDetail } from 'store/slices/video-slice';
-import { toggleFavorites } from 'store/thunks/video-thunk';
+import { fetchVideo, toggleFavorites } from 'store/thunks/video-thunk';
 import './VideoLayout.scss';
 
-interface VideoLayoutProps {
-  video: VideoItemDetail;
-}
-
-const VideoLayout: React.FC<VideoLayoutProps> = ({ video }) => {
+const VideoLayout: React.FC = () => {
   const { refreshToken } = useAppSelector((state) => state.user);
-  const { dispatch } = useAppDispatch();
+  const { dispatchThunk, dispatch, data, setData, loaded } =
+    useAppDispatch<VideoItemDetail | null>(null);
 
-  const [videoTree, setVideoTree] = useState(video);
-
+  const { id } = useParams<{ id: string }>();
   const history = useHistory();
 
+  useEffect(() => {
+    dispatchThunk(fetchVideo(id));
+  }, [dispatchThunk, id]);
+
   const toggleFavoritesHandler = async () => {
-    setVideoTree((prev) => ({
-      ...prev,
-      data: {
-        ...prev.data,
-        favorites: prev.data.isFavorite
-          ? prev.data.favorites - 1
-          : prev.data.favorites + 1,
-        isFavorite: !prev.data.isFavorite,
-      },
-    }));
+    if (!data) return;
+
+    setData((prevState) => {
+      const prev = prevState as VideoItemDetail;
+      return {
+        ...prev,
+        data: {
+          ...prev.data,
+          favorites: prev.data.isFavorite
+            ? prev.data.favorites - 1
+            : prev.data.favorites + 1,
+          isFavorite: !prev.data.isFavorite,
+        },
+      };
+    });
 
     if (!refreshToken) {
       return history.push('/auth');
     }
 
-    const data = await dispatch(toggleFavorites(video._id!));
+    const result = await dispatch(toggleFavorites(data._id!));
 
-    setVideoTree((prev) => ({
-      ...prev,
-      data: {
-        ...prev.data,
-        favorites: data.favorites,
-        isFavorite: data.isFavorite,
-      },
-    }));
+    setData((prevState) => {
+      const prev = prevState as VideoItemDetail;
+      return {
+        ...prev,
+        data: {
+          ...prev.data,
+          favorites: result.favorites,
+          isFavorite: result.isFavorite,
+        },
+      };
+    });
   };
 
   return (
     <div className="video-layout">
       <div className="video-layout__video">
-        <VideoTree tree={videoTree} history={videoTree.history} />
+        <Loader on={!loaded} />
+        {data && <VideoTree tree={data} history={data.history} />}
       </div>
-      <div className="video-layout__info">
-        <div className="video-layout__header">
-          <div className="video-layout__title">
-            <h2>{videoTree.info.title}</h2>
-            <div className="video-layout__favorites">
-              <VideoFavorites
-                favorites={videoTree.data.favorites}
-                active={videoTree.data.isFavorite}
-                onClick={toggleFavoritesHandler}
-              />
+      {data && (
+        <div className="video-layout__info">
+          <div className="video-layout__info__header">
+            <div className="video-layout__title">
+              <h2>{data.info.title}</h2>
+              <div className="video-layout__favorites">
+                <VideoFavorites
+                  favorites={data.data.favorites}
+                  active={data.data.isFavorite}
+                  onClick={toggleFavoritesHandler}
+                />
+              </div>
+            </div>
+            <div className="video-layout__createdAt">
+              <VideoTimestamp createdAt={data.createdAt} />
             </div>
           </div>
-          <div className="video-layout__createdAt">
-            <VideoTimestamp createdAt={videoTree.createdAt} />
+          <div
+            className="video-layout__creator"
+            onClick={() => history.push(`/channel/${data.info.creator}`)}
+          >
+            <Avatar src={data.info.creatorInfo.picture} button />
+            <h3 className="link">{data.info.creatorInfo.name}</h3>
+          </div>
+          <VideoTags tags={data.info.tags} />
+          <div className="video-layout__detail">
+            <VideoViews video={data} />
+            <VideoDuration
+              minDuration={data.info.minDuration}
+              maxDuration={data.info.maxDuration}
+            />
+            <p className="video-layout__description">{data.info.description}</p>
           </div>
         </div>
-        <div
-          className="video-layout__creator"
-          onClick={() => history.push(`/channel/${videoTree.info.creator}`)}
-        >
-          <Avatar src={videoTree.info.creatorInfo.picture} button />
-          <h3 className="link">{videoTree.info.creatorInfo.name}</h3>
-        </div>
-        <VideoTags tags={videoTree.info.tags} />
-        <div className="video-layout__detail">
-          <VideoViews video={videoTree} />
-          <VideoDuration
-            minDuration={videoTree.info.minDuration}
-            maxDuration={videoTree.info.maxDuration}
-          />
-          <p className="video-layout__description">
-            {videoTree.info.description}
-          </p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/pages/Video/VideoPage.tsx
+++ src/pages/Video/VideoPage.tsx
@@ -1,28 +1,7 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router';
-
-import Loader from 'components/Video/Player/UI/Loader/Loader';
 import VideoLayout from 'components/Video/Layout/VideoLayout';
-import { useAppDispatch } from 'hooks/store-hook';
-import { VideoItemDetail } from 'store/slices/video-slice';
-import { fetchVideo } from 'store/thunks/video-thunk';
 
 const VideoPage: React.FC = () => {
-  const { dispatchThunk, data, loaded } =
-    useAppDispatch<VideoItemDetail | null>(null);
-
-  const { id } = useParams<{ id: string }>();
-
-  useEffect(() => {
-    dispatchThunk(fetchVideo(id));
-  }, [dispatchThunk, id]);
-
-  return (
-    <>
-      <Loader on={!loaded} />
-      {data && <VideoLayout video={data} />}
-    </>
-  );
+  return <VideoLayout />;
 };
 
 export default VideoPage;
