6c5e575db0541a0654eed5b74daf911124a53c13
JK <dlord4242@gmail.com>
2022-07-16 21:41:25 +0900

    Updated main

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <small className="footer__copyright">
-        Copyright &copy; {copyrightYear} WatchTrees
+        &copy; {copyrightYear} WatchTrees.
       </small>
     </footer>
   );
